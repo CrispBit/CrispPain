b28f61a86f2a2a39e00bbacaa84f30858eb146a5
Matthew Tran <0e4ef622@gmail.com>
2017-09-29 13:35:02 -0500

    remove trailing whitespaces

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 int main(int argc, char** argv) {
     // select file and get path
     QApplication app(argc, argv);
-    
+
     MainMenu window;
     window.resize(250, 150);
     window.setWindowTitle("Simple menu");
     window.show();
-    
+
     return app.exec();
 }
